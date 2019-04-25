d967f9d71d5f61fe44e9fa8f83f742908232c518
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-04-25 15:13:08 +0200

    Added heading and body fonts

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from '@/layouts/Default.vue'
+import style from '@/scss/style.scss'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  // Add heading font
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Merriweather',
+  })
+
+  // Add text font
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro',
+  })
 }
