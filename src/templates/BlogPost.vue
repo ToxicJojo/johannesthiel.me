914d8f15cf2050a3294b05b94b3b3e6ddc9218b8
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-04-26 14:40:40 +0200

    Added a basic blog post page

---
+++ src/templates/BlogPost.vue
@@ -0,0 +1,43 @@
+<template lang='pug'>
+  Layout
+    section
+      h1 {{ post.title }} 
+      time {{ formattedDate }}
+      div(v-html='post.content')
+</template>
+
+
+<page-query>
+
+query BlogPost ($path: String!) {
+  post: blogPost (path: $path) {
+    title
+    content
+    date
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: 'BlogPost',
+  computed: {
+    post () {
+      return this.$page.post
+    },
+        formattedDate () {
+      return new Date(this.post.date).toLocaleDateString()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+section {
+  max-width: 1024px;
+  margin: auto;
+  padding: 30px;
+}
+
+</style>
