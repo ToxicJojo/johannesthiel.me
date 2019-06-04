b0cba49901fb25d73834f0db556e1fbc774e42e2
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-06-04 17:51:35 +0200

    Serve build step

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ const clean = require('./gulp/clean')
 const { cssProd, cssDev, cssWatch } = require('./gulp/css')
 const { jsProd, jsDev, jsWatch } = require('./gulp/js')
 const { html, htmlWatch } = require('./gulp/html')
+const serve = require('./gulp/serve')
 
 module.exports = {
   buildProd: series(clean, html, cssProd, jsProd),
   buildDev: series(clean, html, cssDev, jsDev),
-  serve: series(htmlWatch, cssWatch, jsWatch),
+  serve: series(htmlWatch, cssWatch, jsWatch, serve),
 }
