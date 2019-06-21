65f4f73b5e842da4d702bfdf880c04b3776514a2
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-06-21 16:41:38 +0200

    Eleventy (#9)
    * Deleted everything
    
    * Basic eleventy setup
    
    * Build Setup (#10)
    
    * Gulp setup with a task to clean the output dir
    
    * CSS build step
    
    * JS build step
    
    * html build step
    
    * Serve build step
    
    * Base CSS (#11)
    
    * Basic typography styles
    
    * Header css
    
    * Added footer
    
    * Basic project page
    
    * Changed projects layout

---
+++ gulp/js.js
@@ -0,0 +1,48 @@
+const { watch, dest } = require('gulp')
+const babel = require('gulp-babel')
+const sourcemaps = require('gulp-sourcemaps')
+const rollup = require('rollup-stream')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+
+const scriptsConfig = {
+  entry: './src/assets/scripts/main.js',
+  watchDir: './src/assets/scripts/**/*.js',
+  dest: './dist/js',
+}
+
+const jsProd = () => {
+  return rollup({
+    input: scriptsConfig.entry,
+    format: 'es',
+  })
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(babel())
+    .pipe(dest(scriptsConfig.dest)) 
+}
+
+const jsDev = () => {
+  return rollup({
+    input: scriptsConfig.entry,
+    format: 'es',
+    sourcemap: true,
+  })
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(dest(scriptsConfig.dest))
+}
+
+const jsWatch = (done) => {
+  watch(scriptsConfig.watchDir, jsDev)
+  done()
+}
+
+module.exports = {
+  jsProd,
+  jsDev,
+  jsWatch,
+}
